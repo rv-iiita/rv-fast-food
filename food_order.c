9ce7623d6c706a86a81b1f9d5d27844b141c9536
RV <80811196+rv-iiita@users.noreply.github.com>
2021-10-03 23:59:56 +0530

    Update food_order.c

--- food_order.c
+++ food_order.c
@@ -237,3 +237,262 @@ struct node* delete(int data, struct node *head, struct node* tail)
     }
     return head;
 }
+int deleteadmin()
+{
+    printf("\n\t\t\t\t\tEnter serial no. of the food item which is to be deleted: ");
+    int num;
+    scanf("%d", &num);
+
+    struct node* temp = heada;
+    while (temp != NULL)
+    {
+        if (temp->data == num)
+        {
+            heada = delete(num, heada, taila);
+            return 1;
+        }
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+int deletecustomer()
+{
+    printf("\n\t\t\t\t\tEnter serial no. of the food item which is to be deleted: ");
+    int num;
+    scanf("%d", &num);
+
+    struct node* temp = headc;
+    while (temp != NULL)
+    {
+        if (temp->data == num)
+        {
+            headc = delete(num, headc, tailc);
+            return 1;
+        }
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+void displaybill()
+{
+    displayList(headc);
+    struct node *temp = headc;
+    float total_price = 0;
+    while (temp != NULL)
+    {
+        total_price += temp->price;
+        temp = temp->next;
+    }
+
+    printf("\t\t\t\t\t\t\tTotal price: %0.02f\n", total_price);
+
+}
+
+struct node* deleteList(struct node* head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        struct node* temp = head;
+        while (temp->next != 0)
+        {
+            temp = temp->next;
+            free(temp->prev);
+        }
+        free(temp);
+        head = NULL;
+    }
+
+    return head;
+}
+
+void admin()
+{
+    printf("\n\t\t\t\t\t  ----------------------------------------------\n");
+    printf("\t\t\t\t\t\t\t    ADMIN SECTION\n");
+    printf("\t\t\t\t\t   ----------------------------------------------\n");
+    while (1)
+    {
+        adminmenu();
+
+        int opt;
+        scanf("%d", &opt);
+
+        if (opt == 5)
+            break;
+
+        switch (opt)
+        {
+        case 1:
+            displayList(head_s);
+            break;
+        case 2:
+
+            printf("\n\t\t\t\t\t\t\tEnter serial no. of the food item: ");
+            int num, flag = 0;
+            char name[50];
+            float price;
+            scanf("%d", &num);
+
+            struct node *temp = heada;
+
+            while (temp != NULL)
+            {
+                if (temp->data == num)
+                {
+                    printf("\n\t\t\t\t\t\tFood item with given serial number already exists!!\n\n");
+                    flag = 1;
+                    break;
+                }
+                temp = temp->next;
+            }
+
+            if (flag == 1)
+                break;
+
+            printf("\t\t\t\t\t\t\tEnter food item name: ");
+            scanf("%s", name);
+            printf("\t\t\t\t\t\t\tEnter price: ");
+            scanf("%f", &price);
+            heada = createadmin(heada, num, name, price);
+            printf("\n\t\t\t\t\t\t\tNew food item added to the list!!\n\n");
+            break;
+        case 3:
+            if (deleteadmin())
+            {
+                printf("\n\t\t\t\t\t\t### Updated list of food items menu ###\n");
+                displayList(heada);
+            }
+            else
+                printf("\n\t\t\t\t\t\tFood item with given serial number doesn't exist!\n\n");
+
+            break;
+        case 4:
+            printf("\n\t\t\t\t\t\t\t   ### Order menu ###\n");
+            displayList(heada);
+            break;
+
+        default:
+            printf("\n\t\t\t\t\t\tWrong Input !! PLease choose valid option\n");
+            break;
+        }
+    }
+}
+
+void customer()
+{
+    int flag = 0, j = 1;
+    char ch;
+    printf("\n\t\t\t\t\t  ----------------------------------------------\n");
+    printf("\t\t\t\t\t\t\t    CUSTOMER SECTION\n");
+    printf("\t\t\t\t\t   ----------------------------------------------\n");
+    while (1)
+    {
+        customermenu();
+
+        int opt;
+        scanf("%d", &opt);
+
+        if (opt == 5)
+            break;
+
+        switch (opt)
+        {
+        case 1:
+            displayList(heada);
+            printf("\n\t\t\t\t\t\tEnter number corresponding to the item you want to order: ");
+            int n;
+            scanf("%d", &n);
+            printf("\t\t\t\t\t\tEnter quantity: ");
+            int quantity;
+            scanf("%d", &quantity);
+            headc = createcustomer(headc, n, quantity);
+            break;
+        case 2:
+            printf("\n\t\t\t\t\t\t\t  ### List of ordered items ###\n");
+            displayList(headc);
+            break;
+        case 3:
+            if (deletecustomer())
+            {
+                printf("\n\t\t\t\t\t\t### Updated list of your ordered food items ###\n");
+                displayList(headc);
+            }
+            else
+                printf("\n\t\t\t\t\t\tFood item with given serial number doesn't exist!!\n");
+            break;
+        case 4:
+            calculatetotsales();
+            printf("\n\t\t\t\t\t\t\t  ### Final Bill ###\n");
+            displaybill();
+            headc = deleteList(headc);
+            printf("\n\t\t\t\t\t\tPress any key to return to main menu:\n\t\t\t\t\t\t");
+            fflush(stdin);
+            ch = fgetc(stdin);
+            flag = 1;
+            break;
+
+        default:
+            printf("\n\t\t\t\t\t\tWrong Input !! PLease choose valid option\n");
+            break;
+        }
+        if (flag == 1)
+            break;
+    }
+}
+
+void mainnenu()
+{
+    printf("\n                                 **************************************************************************\n");
+    printf("                                                     WELCOME TO RV' DA FOOD POINT\n");
+    printf("                                 **************************************************************************\n\n\n");
+    printf("\t\t\t\t\t\t\t1. ADMIN SECTION--> \n");
+    printf("\t\t\t\t\t\t\t2. CUSTOMER SECTION--> \n");
+    printf("\t\t\t\t\t\t\t3. Exit--> \n\n");
+    printf("\t\t\t\t\t\t\tEnter Your Choice --->");
+}
+
+int main()
+{
+    heada = createadmin(heada, 1, "Veg momos", 50);
+    heada = createadmin(heada, 2, "Manchow Soup", 200);
+    heada = createadmin(heada, 3, "Manchurian Noodles", 150);
+    heada = createadmin(heada, 4, "Egg Fried Rice", 180);
+    heada = createadmin(heada, 5, "Dosa", 80);
+
+    while (1)
+    {
+        mainnenu();
+        int choice;
+        scanf("%d", &choice);
+
+        if (choice == 3)
+        {
+            printf("\n\n\t\t\t\t\t\t\t**********Thank you!!**********\n");
+            break;
+        }
+
+        switch (choice)
+        {
+        case 1:
+            admin();
+            break;
+        case 2:
+            customer();
+            break;
+        case 3:
+            break;
+
+        default:
+            printf("\n\t\t\t\t\t\tWrong Input !! PLease choose valid option\n");
+            break;
+        }
+    }
+}
