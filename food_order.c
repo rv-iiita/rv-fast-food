30603557a88d0ff569566638742bcc7df1977051
RV <80811196+rv-iiita@users.noreply.github.com>
2021-10-03 23:58:55 +0530

    Update food_order.c

--- food_order.c
+++ food_order.c
@@ -97,3 +97,143 @@ struct node* createcustomer(struct node *head, int data, int quantity)
         if (temp == NULL)
             headc = tailc = newnode;
         else
+        {
+                        while (temp->next != NULL)
+                temp = temp->next;
+
+            temp->next = newnode;
+            newnode->prev = tailc;
+            tailc = newnode;
+        }
+
+
+    }
+    else
+    {
+        printf("\n\t\t\t\t\t\t\tThis item is not present in the menu!\n");
+    }
+    return headc;
+}
+
+void displayList(struct node *head)
+{
+    struct node *temp1 = head;
+    if (temp1 == NULL)
+    {
+        printf("\n\t\t\t\t\t\t\t\tList is empty!!\n\n");
+    }
+    else
+    {
+        printf("\n");
+        while (temp1 != NULL)
+        {
+            if (temp1->quantity == 0)
+                printf("\t\t\t\t\t\t\t%d\t%s\t%0.2f\n", temp1->data, temp1->foodname, temp1->price);
+            else
+            {
+                printf("\t\t\t\t\t\t\t%d\t%s\t%d\t%0.2f\n", temp1->data, temp1->foodname, temp1->quantity, temp1->price);
+            }
+
+            temp1 = temp1->next;
+        }
+        printf("\n");
+    }
+
+}
+
+struct node* totalsales(int data, int quantity)
+{
+    newnode = (struct node*)malloc(sizeof(struct node));
+    int flag = 0;
+
+    struct node *temp1 = heada;
+    while (temp1->data != data)
+    {
+        temp1 = temp1->next;
+    }
+
+    newnode->data = data;
+    newnode->price = quantity * (temp1->price);
+    newnode-> quantity = quantity;
+    strcpy(newnode->foodname, temp1->foodname);
+    newnode->next = NULL;
+    newnode->prev = NULL;
+
+    struct node *temp = head_s;
+
+    if (temp == NULL)
+        head_s = newnode;
+    else
+    {
+        while (temp->next != NULL)
+        {
+            if (temp->data == data)
+            {
+                flag = 1;
+                break;
+            }
+            temp = temp->next;
+        }
+
+        if (flag == 1)
+        {
+            temp->quantity += newnode-> quantity;
+            temp->price += newnode->price;
+        }
+        else
+        {
+            temp->next = newnode;
+        }
+    }
+
+    return head_s;
+}
+
+void calculatetotsales()
+{
+    struct node *temp = headc;
+    while (temp != NULL)
+    {
+        head_s = totalsales(temp->data, temp->quantity);
+        temp = temp->next;
+    }
+}
+
+struct node* delete(int data, struct node *head, struct node* tail)
+{
+    if (head == NULL)
+    {
+        printf("\n\t\t\t\t\t\t\tList is empty\n");
+    }
+    else
+    {
+        struct node* temp;
+        if (data == head->data)
+        {
+            temp = head;
+            head = head->next;
+            if (head != NULL)
+                head->prev = NULL;
+            free(temp);
+        }
+        else if (data == tail->data)
+        {
+            temp = tail;
+            tail = tail->prev;
+            tail->next = NULL;
+            free(temp);
+        }
+        else
+        {
+            temp = head;
+            while (data != temp->data)
+            {
+                temp = temp->next;
+            }
+            (temp->prev)->next = temp->next;
+            (temp->next)->prev = temp->prev;
+            free(temp);
+        }
+    }
+    return head;
+}
